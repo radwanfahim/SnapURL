6d6d5718c3fb184025cdb522f653ff2bae941cd7
radwanfahim <88280947+radwanfahim@users.noreply.github.com.>
2026-01-04 22:52:40 +0600

    first commit

---
+++ frontend/app/components/Auth/SignIn.tsx
@@ -0,0 +1,13 @@
+const SignIn = () => {
+  return (
+    <section className="">
+      <div className="container mx-auto max-w-7xl px-4 sm:px-6 ">
+        {/* sign in */}
+        <h1>Sign In</h1>
+        <form></form>
+      </div>
+    </section>
+  );
+};
+
+export default SignIn;
